b750f989675fffccc7197f8e374f340a3272cfa8
Karen Mac <karenbtlai@gmail.com>
2016-11-04 13:28:09 -0400

    fixed scrolling behavior for collapsible #btn-aboutme

---
+++ script.js
@@ -0,0 +1,26 @@
+/*document.addEventListener("DOMContentLoaded", function() {
+	
+});*/
+
+$(document).ready(function (){
+    
+	var isExpanded = $("#btn-aboutme").attr("aria-expanded");
+	isExpanded = false;
+	//console.log(isExpanded);
+
+    $("#btn-aboutme").click(function (){
+
+    	if (isExpanded == false) {
+	        $('html, body').animate({
+	            scrollTop: $("#projects").offset().top -200
+	        }, 1000);
+
+	    isExpanded = true;
+	    //console.log(isExpanded);
+
+	    } else {
+	    	isExpanded = false;
+	    }
+   	});
+
+});
